74a17748a7aca9896f7c020f639d91f548da2714
Bernardo Matuchewski <bernardopgr01@gmail.com>
2025-06-04 14:45:00 -0300

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function CardForm(): JSX.Element {
     if (error) {
       alert(error.message);
     } else if (token) {
-      window.location.href = `ecolife://ecolifemeioambiente.com.br`;
+      window.location.href = `ecolife://orders/${token.id}`;
     }
   };
 
